0cab7b963d41f285ba312bfb200b9179cec9e815
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-07-02 10:21:19 -0700

    ASP Core Net MVC

---
+++ MyAppWeb/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppWeb.Data;
+using MyAppWeb.Models;
+
+namespace MyAppWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories;
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
